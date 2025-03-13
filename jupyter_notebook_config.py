fbfc11f4ec7c4ebe164ecd5e19a79273d7747154
Rok Roškar <rok.roskar@sdsc.ethz.ch>
2025-03-13 12:41:51 +0000

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -8,6 +8,8 @@ c.ServerProxy.servers = {
             'work/streamlit-v2-example/helloworld.py',
             '--server.port', '8501',
             '--browser.serverAddress', '0.0.0.0',
+            '--server.runOnSave', '1',
+            '--server.allowRunOnSave', '1'
         ],
         'port': 8501,
         'timeout': 60
