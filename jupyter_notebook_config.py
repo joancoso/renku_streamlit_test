a94913ba631c5fe1f15cef978e058b0e19d132ec
Cordero <jose.cordero@eawag.ch>
2025-09-24 14:29:57 +0200

    modified requirements and notebook config to match the instructions

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -5,7 +5,7 @@ c.ServerProxy.servers = {
         'command': [
             'streamlit',
             'run',
-            'work/streamlit-v2-example/helloworld.py',
+            'work/von_Borries_space/app.py',
             '--server.port', '8501',
             '--browser.serverAddress', '0.0.0.0',
             '--server.runOnSave', '1',
